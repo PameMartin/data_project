fb7f42a8546d23a572317251353b39c012f701e4
Pamela Martin <pmartin3@academic.rrc.ca>
2025-02-24 14:50:36 -0600

    Adding csv in gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP}
   validates :first_name, :last_name, :email, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
 require 'csv'
 User.destroy_all
 
-filename = Rails.root.join("user_data.csv")
+filename = Rails.root.join("db/user_data.csv")
 csv_data = File.read(filename)
 users = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
 
 users.each do |user|
-  user.create(
+  User.create(
     first_name: user['First Name'],
-    last_name: user['Last Nmae'],
-    gender: user['gender'],
-    email: user['email'],
-    phone: user['phone'],
-    date_of_birth: user['date_of_birth'],
-    job_title: user['jobtitle']
+    last_name: user['Last Name'],
+    gender: user['Genre'],
+    email: user['Email'],
+    phone: user['Phone'],
+    date_of_birth: user['Date of birth'],
+    job_title: user['Job Title']
   )
 end
 
