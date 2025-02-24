4271242502de1b01b4bbaae721fcbcbe84adfd7e
Pamela Martin <pmartin3@academic.rrc.ca>
2025-02-24 14:07:41 -0600

    Writing the code in seeds to create the users from the csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'csv'
+User.destroy_all
+
+filename = Rails.root.join("user_data.csv")
+csv_data = File.read(filename)
+users = CSV.parse(csv_data, headers: true, encoding: 'utf-8')
+
+users.each do |user|
+  user.create(
+    first_name: user['First Name'],
+    last_name: user['Last Nmae'],
+    gender: user['gender'],
+    email: user['email'],
+    phone: user['phone'],
+    date_of_birth: user['date_of_birth'],
+    job_title: user['jobtitle']
+  )
+end
+
+puts "Create #{User.count} users."
