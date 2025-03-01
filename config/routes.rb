8510dade83413c8ad26d556bbc46b7f5ee09c062
Pamela Martin <pmartin3@academic.rrc.ca>
2025-02-28 18:16:58 -0600

    Adding Controller for Destinations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "destination/index"
+  get "destination/show"
   root "users#index"
   resources :users, only: [ :index, :show ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
