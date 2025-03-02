2deae220a3abcb115a34ec7936d47c1e487f7d2b
Pamela Martin <pmartin3@academic.rrc.ca>
2025-03-02 14:41:26 -0600

    Adding associations for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,7 @@
 class Review < ApplicationRecord
+  belongs_to :user
+  belongs_to :trip
+
+  validates :rating, presence: true
+  validates :comment, presence: true, length: { minimum: 10 }
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,5 +2,7 @@ class Trip < ApplicationRecord
   belongs_to :user
   belongs_to :destination
 
+  has_many :reviews, dependent: :destroy
+
   validates :travel_date, presence: true
 end
