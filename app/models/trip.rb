f416c8bcb88af9a0592160538b4d96a00df348ce
Pamela Martin <pmartin3@academic.rrc.ca>
2025-03-05 18:11:03 -0600

    Modified the associations in the trip and users models

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,8 +1,9 @@
 class Trip < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
   belongs_to :destination
 
   has_many :reviews, dependent: :destroy
+  has_many :users, through: :reviews
 
   validates :travel_date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :trips
-  has_many :reviews, dependent: :destroy
+  has_many :reviews, through: :trips, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :first_name, :last_name, :email, presence: true
