486cb36e531b4850afdd516f000e8a8bcef14c73
Pamela Martin <pmartin3@academic.rrc.ca>
2025-03-07 15:09:51 -0600

    Adding simple search

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,14 @@
 class ReviewsController < ApplicationController
   def index
-    @reviews = Review.includes(:user, trip: :destination)
-                      .order(rating: :desc)
+    @reviews = if params[:query].present?
+                  Review.joins(trip: :destination)
+                        .select("reviews.*, destinations.name AS city_name")
+                        .where("LOWER(destinations.name) LIKE LOWER(?)", "%#{params[:query]}%")
+                        .order(rating: :desc)
+    else
+                  Review.includes(:user, trip: :destination)
+                         .order(rating: :desc)
+    end
   end
 
   def show
